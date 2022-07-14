4ab6eb96a632da9e8cad0fa0e9e1e6642ddceabd
Caio Gomide <65629053+caiogomide@users.noreply.github.com>
2022-07-14 15:37:43 -0400

    ADD: header

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,6 +1,18 @@
 from jogo_da_velha_view import JogoDaVelhaView
 
+'''
+    Jogo da Velha
+
+    @version 1.0
+    @author Caio Viegas de Souza Gomide e Geysson Guilherme Veiga Viana
+
+    Trabalho de implementação de três algorítimos para a solução
+    de um problema de Jogo da Velha, apresentado a UFMT como parte 
+    da discipina de Inteligência Artificial, lecionada pelo docente
+    Frederico Santos de Oliveira 
+'''
+
 if __name__ == "__main__":
-    
+
     jogo_da_velha = JogoDaVelhaView()
     jogo_da_velha.mainloop()

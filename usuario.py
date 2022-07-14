2e51910078c43eb10646195a108a8a1df7abf170
Caio Gomide <65629053+caiogomide@users.noreply.github.com>
2022-07-14 15:39:30 -0400

    ADD: comentários aos métodos

--- usuario.py
+++ usuario.py
@@ -6,6 +6,13 @@ class Usuario(Jogador):
 
         self.simbolo = simbolo
 
+ 
+    '''
+    Método responsável pelo Usuário
+    posicionar um símbolo dado uma grade,
+    retorna o melhor posicionamento, no formato
+    (linha, coluna)
+    '''
     def posicionar_simbolo(self, linha, coluna, grade,jogo):
-
-        grade.atualiza_grade(linha, coluna, self.simbolo, jogo)
+        grade.atualiza_grade(linha, coluna, self.simbolo, jogo) 
+        return (linha, coluna)

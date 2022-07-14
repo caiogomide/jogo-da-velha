590bc19ff58d3999daf3d39c0807145592596655
Caio Gomide <65629053+caiogomide@users.noreply.github.com>
2022-07-14 15:34:28 -0400

    ADD: comentários aos métodos

--- grade.py
+++ grade.py
@@ -4,6 +4,12 @@ class Grade:
 
         self.posicoes = self.forma_grade_inicial()
 
+    '''
+    Método responsável por devolver uma grade
+    inicial, vazia, que é composta por nove
+    células, e pode ser acessada no formato
+    grade[linha, coluna]
+    '''
     def forma_grade_inicial(self):
 
         grade = []
@@ -11,12 +17,16 @@ class Grade:
         for linha in range(3):
             linha = [[],[],[]]
             grade.append(linha)
-            
+
         return grade
 
+    '''
+    Método responsável por atualizar a grade
+    ao posicionar um símbolo em determinada
+    posição
+    '''
     def atualiza_grade(self, linha, coluna, simbolo, jogo):
 
         self.posicoes[linha][coluna] = simbolo
         jogo._numero_de_jogadas += 1
-    
     

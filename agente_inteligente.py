d21484fca6999bec09d5fa3683aa6b3baec54228
Caio Gomide <65629053+caiogomide@users.noreply.github.com>
2022-07-14 14:08:29 -0400

    UPDATE: melhorias de identação

--- agente_inteligente.py
+++ agente_inteligente.py
@@ -1,14 +1,12 @@
 from jogador import Jogador
 
-from abc import abstractmethod
-
-
-class AgenteInteligente(Jogador):
+class Usuario(Jogador):
 
     def __init__(self,simbolo):
+
         self.simbolo = simbolo
 
-    @abstractmethod
-    def posicionar_simbolo(self, grade, jogo):
-        pass
+    def posicionar_simbolo(self, linha, coluna, grade,jogo):
+
+        grade.atualiza_grade(linha, coluna, self.simbolo, jogo)
 

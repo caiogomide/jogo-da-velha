989820cf703f20d417c93c08590cc3ebde824369
Caio Gomide <65629053+caiogomide@users.noreply.github.com>
2022-07-14 15:35:56 -0400

    ADD: comentários aos métodos

--- jogo_da_velha_controller.py
+++ jogo_da_velha_controller.py
@@ -1,78 +1,121 @@
-  from charset_normalizer import from_fp
 from minimax import Minimax
-
-
 import simulated_annealing
 from grade import Grade
 from usuario import Usuario
 
-
 class JogoDaVelhaController:
     
     def __init__(self):
+
         self._usuario = Usuario('')
         self._agente_inteligente = self.cria_agente_inteligente()
-        self._numero_de_jogadas = 0
+        self._flag_rodada = 'usuario'
         self._grade_atual = Grade()
+
+        self._numero_de_jogadas = 0
+        
         self._combinacoes_vencedoras = self.get_combinacoes_vencedoras()
         self._ha_vencedor = False
-        self._flag_rodada = 'usuario'
+        
         self._pontuacao_jogador = 0
         self._pontuacao_agente_inteligente = 0
         self._contador_empate = 0
     
-    def cria_agente_inteligente(agente_inteligente):
-
+    '''
+    Método responsável por criar um Agente Inteligente
+    ao receber um parâmetro que o determina, retorna
+    alguma classe herdeira de Agente Inteligente,
+    SimulatedAnnealing ou Minimax
+    '''
+    def cria_agente_inteligente(self):
 
         entrada_valida = False
+
         while entrada_valida != True:
        
             print('[1] Para jogar com o Agente Inteligente Minimax \n[2] Para jogar com o Agente Inteligente Simulated Annealing')
+            
             agente_inteligente = int(input('Digite o código do Agente Inteligente Desejado: '))
+
             if agente_inteligente == 1:
+
                 entrada_valida = True
                 return Minimax('')
+
             elif agente_inteligente == 2:
+
                 entrada_valida = True
                 return simulated_annealing.SimulatedAnnealing('')
+
             else:
+
                 print("Entrada Inválida, tente novamente.")
 
+    '''
+    Método responsável por calcular e retornar
+    a quantidade de espaços já ocupados na
+    grade do jogo.
+    '''
     def quantidade_espacos_posicionados(self):
+
         quantidade_espacos_posicionados = 0
         posicoes_grade = self._grade_atual.posicoes
+
         for linha in posicoes_grade:
             for coluna in linha:
                 if coluna == 'X' or coluna == 'O':
                     quantidade_espacos_posicionados+=1
-        print('posicoes_grade:',posicoes_grade)
+
         return quantidade_espacos_posicionados
 
+    '''
+    Método responsável por cálcular e retornar
+    se há espaços não ocupados em uma grade 
+    determinada
+    '''
     def ha_espacos_vazios(self):
+
         posicoes_grade = self._grade_atual.posicoes
+
         for linha in posicoes_grade:
             for coluna in linha:
                 if coluna == []:
                     return True
+
         return False
 
+    '''
+    Método responsável por checar se houve um empate
+    no Jogo, dado uma grade, retorna um valor booleano
+    '''
     def checa_empate(self):
+
         if self.ha_espacos_vazios() == False and self.checa_vencedores() == '':
             return True
+
         return False
 
-    # Retorna se "X" ou "O" venceram
+    '''
+    Método responsável por checa se há vencedores dado uma grade
+    caso haja, retorna o simbolo do vencedor, caso não,
+    retorna uma string vazia
+    '''
     def checa_vencedores(self):
 
         orientacoes = ['Horizontal', 'Vertical', 'Diagonal']
 
+        # Checa se para cada orientação houve um vencedor
         for orientacao in orientacoes:
+
             # Checa se há vencedores na orientacao dada
             combinacoes_vencedoras_por_orientacao = self._combinacoes_vencedoras[orientacao]
             posicoes_simbolos = self._grade_atual.posicoes
+
             for combinacao_vencedora_orientada in combinacoes_vencedoras_por_orientacao:
+
                 combinacao_x = 0
                 combinacao_o = 0
+
                 # Checa se cada combinação ocorre
                 for i in range(3):
                     linha, coluna = combinacao_vencedora_orientada[i]
@@ -91,10 +134,12 @@ class JogoDaVelhaController:
                     return 'O'
         return ''
 
-
-
-        
-
+    '''
+    Método responsável por determinar quais combinações
+    são vencedoras para cada orientação, retorna um 
+    dicionário contendo as combinações de vitórias
+    para cada orientação
+    '''
     def get_combinacoes_vencedoras(self):
         # Para haver um vencedor, deve haver 3 símbolos consecutivos na horizontal, diagonal ou vertical   
         
@@ -113,21 +158,27 @@ class JogoDaVelhaController:
             'Horizontal':combinacao_vencedora_horizontal,
             'Diagonal':combinacao_vencedora_diagonal
         }
-
         
         return combinacoes_vencedoras_possiveis
     
-    # Devolve a sequencia que venceu o jogo
+    '''
+    Método responsável por determinar, caso haja um vencedor no jogo
+    qual foi a sequência que fez o jogador ganhar, retorna uma lista
+    contendo as posições dos símbolos que levaram a vitória
+    '''
     def sequencia_vencedora(self):
 
         orientacoes = ['Horizontal', 'Vertical', 'Diagonal']
         vencedor = self.checa_vencedores()
+
         for orientacao in orientacoes:
             
             # Checa se há vencedores na orientacao dada
             combinacoes_vencedoras_por_orientacao = self._combinacoes_vencedoras[orientacao]
             posicoes_simbolos = self._grade_atual.posicoes
+
             for combinacao_vencedora_orientada in combinacoes_vencedoras_por_orientacao:
+
                 combinacao_vencedora = 0
                 # Checa se cada combinação ocorre
                 for i in range(3):
@@ -143,16 +194,29 @@ class JogoDaVelhaController:
                 if combinacao_vencedora == 3:
                     return combinacao_vencedora_orientada
                 
-
+    '''
+    Método responsável por aumentar a pontuação do jogador 
+    ou agente inteligente ao ele ganhar, ou aumentar o 
+    contador de empate ao ocorrer um empate, não retorna
+    valores
+    '''
     def atualiza_pontuacao(self):
+
         if self.checa_vencedores() == self._usuario.simbolo:
             self._pontuacao_jogador += 1
+
         elif self.checa_vencedores() == self._agente_inteligente.simbolo:
             self._pontuacao_agente_inteligente += 1
+
         elif self.checa_empate():
             self._contador_empate += 1
 
+    '''
+    Método responsável por resetar o controller do jogo
+    para o estado inicial, não retorna valores
+    '''
     def reset_controller(self):
+
         self._grade_atual = Grade()
         self._ha_vencedor = False
         self._flag_rodada = 'usuario'

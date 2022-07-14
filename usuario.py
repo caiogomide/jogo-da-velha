e83d7e8af5c1f0a80bb330238efe822c5118c354
Caio Gomide <65629053+caiogomide@users.noreply.github.com>
2022-07-14 14:13:10 -0400

    UPDATE: melhorias de indentação

--- usuario.py
+++ usuario.py
@@ -1,10 +1,11 @@
 from jogador import Jogador
 
-
 class Usuario(Jogador):
 
     def __init__(self,simbolo):
+
         self.simbolo = simbolo
 
     def posicionar_simbolo(self, linha, coluna, grade,jogo):
+
         grade.atualiza_grade(linha, coluna, self.simbolo, jogo)

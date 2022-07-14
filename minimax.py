4cdf873c8a02bf9526eb1551a22d4cb324ec8dcb
Caio Gomide <65629053+caiogomide@users.noreply.github.com>
2022-07-14 14:11:46 -0400

    UPDATE: melhorias de indentação

--- minimax.py
+++ minimax.py
@@ -4,22 +4,29 @@ from agente_inteligente import AgenteInteligente
 class Minimax(AgenteInteligente):
 
     def __init__(self,simbolo):
+
         self.simbolo = simbolo
 
     # Implementacao do algoritimo MiniMax
     def minimax(self, grade, maximizando, jogo, profundidade):
 
         pontuacao = self.avaliador(jogo)
-        
+
         # Caso o Agente Inteligente tenha sido bem sucedido e vencido
+
         if(pontuacao == 10):
             return pontuacao 
+
         # Caso o Agente Inteligente tenha sido mal sucedido e perdido
+
         if(pontuacao == -10):
             return pontuacao
+
         # Caso o Agente Inteligente tenha empatado com o Usuário
+
         if (jogo.checa_empate()):
             return 2
+
         # Caso seja o turno do Agente Inteligente
         if maximizando:
 
@@ -29,8 +36,10 @@ class Minimax(AgenteInteligente):
                 for coluna in range(3):
                     # Caso o espaço da grade esteja vazio
                     if grade.posicoes[linha][coluna] == []:
+
                         # Faz o movimento com o simbolo do Agente Inteligente
                         grade.posicoes[linha][coluna] = self.simbolo
+                        
                         # Escolhe o máximo valor para determinação do movimento do jogo
                         melhor_opcao = max(melhor_opcao, self.minimax(grade, False, jogo, profundidade+1))
                         # Remove o movimento hipotético feito para análise
@@ -54,18 +63,17 @@ class Minimax(AgenteInteligente):
 
             return melhor_opcao
 
-
-
-
     # Avalia as condicoes de vitoria, derrota ou empate
     def avaliador(self, jogo):
        
         # Analisa vitoria do Usuario
         if jogo.checa_vencedores() == jogo._usuario.simbolo:
             return -10
+
         # Analisa vitoria do Agente Inteligente
         if jogo.checa_vencedores() == self.simbolo:
             return 10
+            
         # Caso ninguem tenha ganhado o jogo
         return 0
 
@@ -87,7 +95,6 @@ class Minimax(AgenteInteligente):
                     # Caso o movimento avaliado seja melhor que o melhor movimento, este é o melhor
                     if(avaliacao > melhor_opcao):
                         melhor_posicionamento = (linha, coluna)
-                        
                         melhor_opcao = avaliacao
        
         grade.atualiza_grade(melhor_posicionamento[0], melhor_posicionamento[1], self.simbolo,jogo)

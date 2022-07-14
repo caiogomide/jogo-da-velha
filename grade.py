9bb2e42e1ea7dc0e0d06713cf662a22bab17985b
Caio Gomide <65629053+caiogomide@users.noreply.github.com>
2022-07-14 14:09:48 -0400

    UPDATE: melhorias de indentação

--- grade.py
+++ grade.py
@@ -1,17 +1,21 @@
 class Grade:
 
-    def __init__(self):
-        
+    def __init__(self):   
+
         self.posicoes = self.forma_grade_inicial()
 
     def forma_grade_inicial(self):
+
         grade = []
+
         for linha in range(3):
             linha = [[],[],[]]
             grade.append(linha)
+            
         return grade
 
     def atualiza_grade(self, linha, coluna, simbolo, jogo):
+
         self.posicoes[linha][coluna] = simbolo
         jogo._numero_de_jogadas += 1
     

31af4e73c5b92f107f38c9b8b10e4263dce9cf92
Caio Gomide <65629053+caiogomide@users.noreply.github.com>
2022-07-13 23:13:05 -0400

    UPDATE: Adição de conceito de número de jogadas
    Adição de tracking de número de jogadas a cada atualização da grade

--- grade.py
+++ grade.py
@@ -11,6 +11,8 @@ class Grade:
             grade.append(linha)
         return grade
 
-    def atualiza_grade(self, linha, coluna, simbolo):
+    def atualiza_grade(self, linha, coluna, simbolo, jogo):
         self.posicoes[linha][coluna] = simbolo
+        jogo._numero_de_jogadas += 1
+    
     
